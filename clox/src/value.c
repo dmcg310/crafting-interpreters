e6dbef723edc11e0ec0d063fb6576ea7cae17bb1
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-12-28 15:32:20 +0000

    feat: equality & comparison operators

--- clox/src/value.c
+++ clox/src/value.c
@@ -27,7 +27,6 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
-
   switch (value.type) {
   case VAL_BOOL:
     printf(AS_BOOL(value) ? "true" : "false");
@@ -38,5 +37,20 @@ void printValue(Value value) {
   case VAL_NUMBER:
     printf("%g", AS_NUMBER(value));
     break;
+  }
+}
+
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type)
+    return false;
+  switch (a.type) {
+  case VAL_BOOL:
+    return AS_BOOL(a) == AS_BOOL(b);
+  case VAL_NIL:
+    return true;
+  case VAL_NUMBER:
+    return AS_NUMBER(a) == AS_NUMBER(b);
+  default:
+    return false; // should be unreachable
   }
 }
