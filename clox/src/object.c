5ff9f12c8b6bb5129a8763fe8723e50f99fe5f7b
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2024-01-10 20:15:42 +0000

    feat: native functions

--- clox/src/object.c
+++ clox/src/object.c
@@ -19,14 +19,19 @@ static Obj *allocateObject(size_t size, ObjType type) {
   return object;
 }
 
-ObjFunction* newFunction() {
-  ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+ObjFunction *newFunction() {
+  ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
   function->name = NULL;
   initChunk(&function->chunk);
 
   return function;
+}
 
+ObjNative *newNative(NativeFn function) {
+  ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+  native->function = function;
+  return native;
 }
 
 static ObjString *allocateString(char *chars, int length, uint32_t hash) {
@@ -75,7 +80,7 @@ ObjString *copyString(const char *chars, int length) {
   return allocateString(heapChars, length, hash);
 }
 
-static void printFunction(ObjFunction* function) {
+static void printFunction(ObjFunction *function) {
   if (function->name == NULL) {
     printf("<script>");
     return;
@@ -86,8 +91,11 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
-  case OBJ_FUNCTION: 
+  case OBJ_FUNCTION:
     printFunction(AS_FUNCTION(value));
+    break;
+  case OBJ_NATIVE:
+    printf("<native fn>");
     break;
   case OBJ_STRING:
     printf("%s", AS_CSTRING(value));
